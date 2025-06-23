38d6b7a6aad5a84e2f47e032619f11db7acf1db6
Austill <cutlerasutin5@gmail.com>
2025-06-23 17:55:59 +0300

    update  milestone 4

--- _config.js
+++ _config.js
@@ -1,14 +1,10 @@
-// // var config = {}
+var config = {}
 
-// // // Update to have your correct username and password
-// // config.mongoURI = {
-// //     production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-// //     development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-// //     test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-// // }
-//module.exports = config;
-module.exports = {
-  dbUrl: 'mongodb+srv://austin:Misarobt1@cluster1.ynxgjwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1'
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://austin:Misarobt1@cluster1.ynxgjwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1',
+    development: 'mongodb+srv://austin:Misarobt1@cluster1.ynxgjwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1',
+    test: 'mongodb+srv://austin:Misarobt1@cluster1.ynxgjwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1',
 }
-// };
- 
+module.exports = config;
+
